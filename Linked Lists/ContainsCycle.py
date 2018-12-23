a3049167eb5087bf5cce5a12d31ceebb78ab90b4
Aman Kishore <amankishore105@gmail.com>
2018-12-23 02:25:48 -0500

    Update ContainsCycle.py

--- Linked Lists/ContainsCycle.py
+++ Linked Lists/ContainsCycle.py
@@ -1,10 +1,10 @@
 '''
 Contains Cycle
     Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean 
-    indicating whether the list contains a cycle.
+    indicating whether the list contains a cycle. Return head
 '''
 
-def contains_cycle(head):
+def cycle_beginning(head):
     slow = head  # Start runners at head
     fast = head
 
@@ -12,6 +12,37 @@ def contains_cycle(head):
         slow = slow.next
         fast = fast.next.next
         if fast is slow:  # Fast reaches slow
-            return True
+            break
+    if fast is None or fast.next is None:
+        return None
+    
+    slow = head
+    while slow != fast:  # Finds the beginning
+        fast = fast.next
+        slow = slow.next
 
-    return False  # No cycle reached end
+    return fast  # Beginning of cycle
+
+
+
+
+def removeLoop(head, slow):
+    ptr1 = ptr2 = slow
+    k = 1
+    while ptr1.next != ptr2:  # Count the number of nodes in the cycle
+        ptr1 = ptr1.next
+        k += 1
+    
+    ptr1 = ptr2 = head
+    for _ in range(k): # Move 
+        ptr2 = ptr2.next
+    
+    while ptr2 != ptr1: # Move both pointers at the same place they will meet at loop starting node
+        ptr1 = ptr1.next
+        ptr2 = ptr2.next
+
+    ptr2 = ptr2.next
+    while(ptr2.next != ptr1): # Get pointer to the last node before the start
+        ptr2 = ptr2.next
+    
+    ptr2.next = None  # delete the loop

8b56536d5c36f96574e437fc5ea6d75e978477ed
Aman Kishore <amankishore105@gmail.com>
2018-12-23 22:49:13 -0500

    Create WhichAppearsTwice.py

---
+++ Combinatorics/WhichAppearsTwice.py
@@ -0,0 +1,9 @@
+'''
+Appears Twice
+    Find the number that appears twice in a list of n+1 elements with values from 0 to n
+'''
+
+def find_repeat(nums):
+    n = len(nums) - 1
+    sum_wo_duplicate = (n(n - 1))/2
+    return sum(nums) - sum_wo_duplicate

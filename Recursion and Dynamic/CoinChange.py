2db5c78c1b848265179cd5e388db57011a2505f8
Aman Kishore <amankishore105@gmail.com>
2018-12-27 17:12:54 -0500

    Update CoinChange.py

--- Recursion and Dynamic/CoinChange.py
+++ Recursion and Dynamic/CoinChange.py
@@ -4,13 +4,13 @@ Coin Change:
 '''
 
 
-def change_possibilities(amount, change):
+def change_possibilities(amount, coins):
     
-    # Calculate the number of ways to make change
+    # Calculate the number of ways to make coins
     memo = [0] * (amount + 1)
     memo[0] = 1 # Number of ways to make 0 cents
     
-    for change in change: # Go through every coin
+    for change in coins: # Go through every coin
         for i in range(change, len(memo)):  # From the coin till the end, use the previous values
             memo[i] += memo[i - change]     # found to determine the number of coins needed
 

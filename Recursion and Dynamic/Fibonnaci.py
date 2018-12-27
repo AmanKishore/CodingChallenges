39d3a98d368c69d4e244e4df8f10d4a2d6387ba2
Aman Kishore <amankishore105@gmail.com>
2018-12-27 14:37:22 -0500

    Update Fibonnaci.py

--- Recursion and Dynamic/Fibonnaci.py
+++ Recursion and Dynamic/Fibonnaci.py
@@ -13,3 +13,21 @@ def fib(n):
     else:
         memo[n] = fib(n-1) + fib(n-2) # Store the newest discovered number
     return memo[n]
+
+ def fib(n):
+    if n < 0:
+        raise ValueError('Index was negative. No such thing as a '
+                         'negative index in a series.')
+    elif n in [0, 1]:
+        return n
+
+    # Bottom Up fibonacci serie
+    prev_prev = 0  # 0th fibonacci
+    prev = 1       # 1st fibonacci
+
+    for _ in range(n - 1):
+        current = prev + prev_prev
+        prev_prev = prev
+        prev = current
+
+    return current

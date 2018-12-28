edea2fbeec6945ffbd3bfab4c1b5b8542229ce4d
Aman Kishore <amankishore105@gmail.com>
2018-12-28 10:13:29 -0500

    Update RotateMatrix.py

--- Cracking the Coding Interview/Arrays and Strings/RotateMatrix.py
+++ Cracking the Coding Interview/Arrays and Strings/RotateMatrix.py
@@ -5,17 +5,17 @@ Rotate Matrix:
 
 def rotate_matrix(matrix):
     for layer in range(len(matrix) // 2):
-        first, last = layer, len(matrix) - layer - 1
-        for i in range(first, last):
-            offset = i - first
+        last = len(matrix) - layer - 1 # The last element in the row that needs to be rotated
+        for i in range(layer, last):
+            offset = i - layer # The element in that layer
             # save top
-            top = matrix[first][i]
+            top = matrix[layer][i]
 
             # left -> top
-            matrix[first][i] = matrix[last - offset][first]
+            matrix[layer][i] = matrix[last - offset][layer]
 
             # bottom -> left
-            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last - offset][layer] = matrix[last][last - offset]
 
             # right -> bottom
             matrix[last][last - offset] = matrix[i][last]

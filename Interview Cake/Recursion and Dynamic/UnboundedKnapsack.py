b6511ed11bb10e029a7b72c4ec93a513d25fb611
Aman Kishore <amankishore105@gmail.com>
2018-12-27 23:02:14 -0500

    Update UnboundedKnapsack.py

--- Interview Cake/Recursion and Dynamic/UnboundedKnapsack.py
+++ Interview Cake/Recursion and Dynamic/UnboundedKnapsack.py
@@ -7,12 +7,10 @@ def max_duffel_bag_value(cake_tuples, weight_capacity):
     
     best_value = [0] * (weight_capacity + 1)  # Set up the array to find max values
     
-    for cake in cake_tuples:
-        weight = cake[0]
-        value = cake[1]
+    for weight, value in cake_tuples:
         if weight is 0 and value > 0: # Infinite cakes!
             return float('inf')
-        for i in range(cake[0], len(best_value)): # Update the value at each weight with the newest cake
+        for i in range(weight, len(best_value)): # Update the value at each weight with the newest cake
             best_value[i] = max(best_value[i], value + best_value[i - weight]) # If the previously found value is better  
 
     return best_value[weight_capacity]

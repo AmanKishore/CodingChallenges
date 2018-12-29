f8f6dc8fd53ba5bba00b2bad093c7ce76f4db755
Aman Kishore <amankishore105@gmail.com>
2018-12-28 22:45:16 -0500

    Update Intersection.py

--- Cracking the Coding Interview/Linked List/Intersection.py
+++ Cracking the Coding Interview/Linked List/Intersection.py
@@ -1,21 +1,23 @@
 '''
 Intersection:
-    Deterimin if two linked lists intersect
+    Deterimine if and where two linked lists intersect
 '''
 
 def intersection(list1, list2):
-    if list1.tail is not list2.tail:
+    if list1.tail is not list2.tail:  # Checking whether the tails are equal
         return False
 
-    shorter = list1 if len(list1) < len(list2) else list2
-    longer = list2 if len(list1) < len(list2) else list1
+    s = list1 if len(list1) < len(list2) else list2
+    l = list2 if len(list1) < len(list2) else list1
 
-    diff = len(longer) - len(shorter)
+    diff = len(l) - len(s) # Finding the difference between the list sizes
+
+    shorter, longer = s.head, l.head
 
     for _ in range(diff):
-        longer = longer.next
+        longer = longer.next # Move the longer diff steps forward to match the length
 
-    while shorter is not longer:
+    while shorter is not longer: # Keep going through until intersection is found
         shorter = shorter.next
         longer = longer.next
 

145daae8756af2211a898ddc01cdf969614b984c
Aman Kishore <amankishore105@gmail.com>
2018-12-30 18:51:50 -0500

    Create TripleStep.py

---
+++ Cracking the Coding Interview/Recursion and Dynamic Programming/TripleStep.py
@@ -0,0 +1,17 @@
+'''
+Triple Step:
+    A child can run up and hop 1, 2, or 3 steps at a time. How many ways can it make it up the stairs.
+'''
+
+memo = {0 : 1, 1 : 1, 2 : 2}
+
+def step(n):
+
+    # Compute the nth Fibonacci number
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = step(n-1) + step(n-2) + step(n-3) # Store the newest discovered number
+    return memo[n]
+
+print(step(4))

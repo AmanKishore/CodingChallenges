2bcf0d0eeca73ee5fceb5c35329b2d9f41228e5d
Aman Kishore <amankishore105@gmail.com>
2018-12-30 16:23:42 -0500

    Create CheckSubtree.py

---
+++ Cracking the Coding Interview/Trees and Graphs/CheckSubtree.py
@@ -0,0 +1,28 @@
+'''
+Check SubTree:
+    Determine if t2 is a subtree of t1
+'''
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def preOrder(node, s): # Gets the preorder traveral string adding an X for None
+	if node == None:
+		s = s + 'X'
+		return
+	s = s + str(node.data) # Add root
+	preOrder(node.left, s) # Add left node
+	preOrder(node.right, s) # Add right node
+
+def containsTree(t1, t2):
+	s1 = ''
+	s2 = ''
+
+	preOrder(t1, s1)
+	preOrder(t2, s2)
+
+	return s2 in s1 # Check if s2 is a substring of s1

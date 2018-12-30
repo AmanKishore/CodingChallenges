7a9e4acde1897957382da15841fc742832bc562f
Aman Kishore <amankishore105@gmail.com>
2018-12-30 15:14:07 -0500

    Update FirstCommonAncestor.py

--- Cracking the Coding Interview/Trees and Graphs/FirstCommonAncestor.py
+++ Cracking the Coding Interview/Trees and Graphs/FirstCommonAncestor.py
@@ -8,17 +8,17 @@ def findLCA(root, n1, n2):
     if root is None: # If you reach the end of the tree 
         return None
   
-    # If either n1 or n2 matches with root's key, report 
-    #  the presence by returning root (Note that if a key is 
-    #  ancestor of other, then the ancestor key becomes LCA 
-    if root.key == n1 or root.key == n2: # Check to see if we have hit a node we are trying to find, then set the left/right
+    # If either n1 or n2 matches with root's value, report 
+    #  the presence by returning root (Note that if a value is 
+    #  ancestor of other, then the ancestor value becomes LCA 
+    if root.value == n1 or root.value == n2: # Check to see if we have hit a node we are trying to find, then set the left/right
         return root  
   
-    # Look for keys in left and right subtrees 
+    # Look for values in left and right subtrees 
     left_lca = findLCA(root.left, n1, n2)  
     right_lca = findLCA(root.right, n1, n2) 
   
-    # If both of the above calls return Non-NULL, then one key 
+    # If both of the above calls return Non-NULL, then one value 
     # is present in once subtree and other is present in other, 
     # So this node is the LCA 
     if left_lca and right_lca: # Only if both left and right are found and have a value then return the root
